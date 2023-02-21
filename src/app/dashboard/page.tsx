fed48964a2d242ab4beb1831c71315dacdc26bea
Daniel Filgueira <dfsdireito95@gmail.com>
2023-02-21 06:58:53 +0000

    Update 16 files

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const metadata = {
+  title: "My Page Title",
+};
+
+export default function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const metadata = {
+  title: "My Page Title",
+};
+
+export default function Page() {
+  return <h1>Home</h1>;
+}
