4ab5bfd6db37bc9acd0b91bbb7e87fb50bafe99f
tech3br <dfsdireito95@gmail.com>
2023-02-21 06:03:42 -0300

    update two files

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Box, VStack } from "@chakra-ui/react";
 import { Icon } from "@chakra-ui/react";
-import { MdHome } from "react-icons/md";
+import { BiHome, BiGridAlt, BiBarChart, BiChat, BiTrendingUp, BiUser, BiBox, BiWrench } from "react-icons/bi";
 
 export default function Aside() {
   return (
     <Box
-      p="4"
+
       bgColor="#161717"
       h="full"
       display="flex"
@@ -14,16 +14,19 @@ export default function Aside() {
       alignItems="center"
       justifyContent="start"
     >
-      <div>Logo</div>
-      <VStack spacing={4} display="flex" align="center" w="full">
+      <Box p="4">Logo</Box>
+      <VStack spacing={2} display="flex" align="center" w="full">
         <Box
           display="flex"
           justifyContent="center"
           alignItems="center"
           w="full"
           h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
         >
-          <Icon as={MdHome} w={6} h={6} color="#8C8C8C" />
+          <Icon _groupHover={{ color: "#fff" }} as={BiHome} w={6} h={6} color="#8C8C8C" />
         </Box>
         <Box
           display="flex"
@@ -31,8 +34,11 @@ export default function Aside() {
           alignItems="center"
           w="full"
           h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
         >
-          <Icon as={MdHome} w={6} h={6} color="#8C8C8C" />
+          <Icon _groupHover={{ color: "#fff" }} as={BiGridAlt} w={6} h={6} color="#8C8C8C" />
         </Box>
         <Box
           display="flex"
@@ -40,10 +46,73 @@ export default function Aside() {
           alignItems="center"
           w="full"
           h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
         >
-          <Icon as={MdHome} w={6} h={6} color="#8C8C8C" />
+          <Icon _groupHover={{ color: "#fff" }} as={BiBarChart} w={6} h={6} color="#8C8C8C" />
         </Box>
-      </VStack>
-    </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          w="full"
+          h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
+        >
+          <Icon _groupHover={{ color: "#fff" }} as={BiChat} w={6} h={6} color="#8C8C8C" />
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          w="full"
+          h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
+        >
+          <Icon _groupHover={{ color: "#fff" }} as={BiTrendingUp} w={6} h={6} color="#8C8C8C" />
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          w="full"
+          h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
+        >
+          <Icon _groupHover={{ color: "#fff" }} as={BiUser} w={6} h={6} color="#8C8C8C" />
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          w="full"
+          h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
+        >
+          <Icon _groupHover={{ color: "#fff" }} as={BiBox} w={6} h={6} color="#8C8C8C" />
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          w="full"
+          h="56px"
+          data-group
+          p="4"
+          _hover={{ cursor: "pointer", bg: '#000', borderTopLeftRadius: 2, borderRadius: 2, borderTopRadius: 2, w: "full" }}
+        >
+          <Icon _groupHover={{ color: "#fff" }} as={BiWrench} w={6} h={6} color="#8C8C8C" />
+        </Box>
+      </VStack >
+    </Box >
   );
 }
